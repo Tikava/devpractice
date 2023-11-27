fc19e5ff3971ec2eb7077139d7515dc5fe16d965
Tikava <wavoleniu@gmail.com>
2023-11-28 01:54:00 +0600

    Update list.py

--- Projects/OOP_and_Classes/Project1/src/list.py
+++ Projects/OOP_and_Classes/Project1/src/list.py
@@ -13,8 +13,8 @@ class TaskList:
             raise InvalidTaskError("The task must be an instance of Task")
         
         tasks = self.get_tasks()
-        tasks.append(task.to_dict())
-        self.file_manager.write_data(json.dumps(tasks))
+        tasks.append(task)
+        self.file_manager.write_data(json.dumps([task.to_dict() for task in tasks]))
         
     def get_tasks(self):
         tasks_str = self.file_manager.read_data()
@@ -41,6 +41,6 @@ class TaskList:
         task = self.get_task(task_title)
         if task:
             updated_tasks = [task for task in tasks if task.get_title() != task_title]
-            self.file_manager.write_data(json.dumps(updated_tasks))
+            self.file_manager.write_data(json.dumps([task.to_dict() for task in updated_tasks]))
         else:
             raise TaskNotFoundError(f"A task with title {task_title} was not found.")
