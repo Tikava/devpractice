2c783f6180b2610936e309d3af980bba402b0d0e
Tikava <wavoleniu@gmail.com>
2023-11-27 18:34:43 +0600

    Update file_manager.py

--- Projects/OOP_and_Classes/Project1/src/file_manager.py
+++ Projects/OOP_and_Classes/Project1/src/file_manager.py
@@ -7,11 +7,11 @@ class FileManager:
         resources_directory = os.path.join(project_directory, 'resources')
         self.file_path = os.path.join(resources_directory, file_name)
 
-    def read_file(self):
+    def read_data(self):
         with open(self.file_path, 'r') as file:
             lines = file.readlines()
         return lines
 
-    def write_file(self, lines):
+    def write_data(self, lines):
         with open(self.file_path, 'w') as file:
             file.writelines(lines)
