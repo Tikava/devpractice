b108adb453f406087ef796dbc88388603c97885d
Tikava <wavoleniu@gmail.com>
2023-11-28 00:08:33 +0600

    Create exceptions directory

---
+++ Projects/OOP_and_Classes/Project1/exceptions/exceptions.py
@@ -0,0 +1,9 @@
+import errno
+
+class FileManagerError(OSError):
+    def __init__(self, file_path, message):
+        super().__init__(errno.ENOENT, message)
+        self.file_path = file_path
+        
+class InvalidTaskError(Exception):
+    pass
