e2ffdb28b53bdee87c0c61ac9e567554bffdf1c8
Tikava <wavoleniu@gmail.com>
2024-01-23 14:13:20 +0600

    Add lecture 33 hw

---
+++ Module7_Data_Science/lecture-33-numpy-part-2/main.py
@@ -0,0 +1,16 @@
+import numpy as np 
+
+array1 = np.array([[1, 2, 3], [4, 5, 6]])
+array2 = np.array([2, 4, 6])
+
+product_array = np.multiply(array1, array2)
+print(product_array)
+
+power_array = np.power(array1, array2)
+print(power_array)
+
+divided_array = np.divide(array1, array2)
+print(divided_array)
+
+add_array = np.add(array1, array2)
+print(add_array)
